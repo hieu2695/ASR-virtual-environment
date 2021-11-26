842a5360ae983326606f2d3da66b47efb7c1e0e3
Hieu Le <51386860+hieu2695@users.noreply.github.com>
2021-11-26 09:45:39 +0700

    Update package_installation_ubuntu.sh

--- package_installation_ubuntu.sh
+++ package_installation_ubuntu.sh
@@ -1,3 +1,10 @@
+# apt-get
+sudo apt-get install libjpeg-dev zlib1g-dev
+sudo pip install -U setuptools
+sudo apt-get install -y p7zip-full
+sudo apt install unzip
+sudo apt-get install gedit -y
+
 # install CUDA 11.3 (Stable)
 pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
@@ -5,50 +12,47 @@ pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu
 sudo apt install build-essential libssl-dev libffi-dev python3-dev -y
 sudo apt-get install tcl-dev tk-dev python-tk python3-tk -y
 sudo pip3 install --upgrade pip
-sudo -H pip3 install numpy --upgrade
-sudo -H pip3 install ipython
-sudo -H pip3 install matplotlib
-sudo -H pip3 install pandas
-sudo -H pip3 install h5py
-sudo -H pip3 install leveldb
-sudo -H pip3 install seaborn
-sudo -H pip3 install tensorflow-gpu
-sudo -H pip3 install Theano
-sudo -H pip3 install keras
-sudo -H pip3 install -U scikit-learn
-sudo -H pip3 install cython
-sudo -H pip3 install opencv-python
-sudo -H pip3 install lmdb
-sudo -H pip3 install sympy
-sudo -H pip3 install pydotplus
-sudo -H pip3 install gpustat
-sudo -H pip3 install xlrd
-sudo -H pip3 install sacred
-sudo -H pip3 install pymongo
-sudo -H pip3 install openpyxl
-sudo -H pip3 install tqdm
-sudo -H pip3 install nltk
-sudo -H pip3 install pyspellchecker
-sudo -H pip3 install -U spacy
-sudo -H pip3 install textacy
-sudo -H pip3 install joblib
-sudo -H pip3 install dataclasses
-sudo -H pip3 install typing
-sudo python3 -m spacy download en
+pip3 install numpy --upgrade
+pip3 install ipython
+pip3 install
+pip3 install matplotlib
+pip3 install pandas
+pip3 install h5py
+pip3 install leveldb
+pip3 install seaborn
+pip3 install tensorflow-gpu
+pip3 install Theano
+H pip3 install keras
+pip3 install -U scikit-learn
+pip3 install cython
+pip3 install opencv-python
+pip3 install lmdb
+pip3 install sympy
+pip3 install pydotplus
+pip3 install gpustat
+pip3 install xlrd
+pip3 install sacred
+pip3 install pymongo
+pip3 install openpyxl
+pip3 install tqdm
+pip3 install nltk
+pip3 install pyspellchecker
+pip3 install -U spacy
+pip3 install textacy
+pip3 install joblib
+pip3 install dataclasses
+pip3 install typing
+python3 -m spacy download en
 
 # install Plotly
-sudo pip3 install plotly
+pip3 install plotly
 
 # install Huggingface packages
-sudo pip3 install transformers
-sudo pip3 install datasets
+pip3 install transformers
+pip3 install datasets
 
-# install image manipulations
+# install audio manipulations
 sudo apt install ffmpeg -y
-sudo -H pip3 install pydub
+pip3 install pydub
 
-# apt-get
-sudo apt-get install -y p7zip-full
-sudo apt install unzip
-sudo apt-get install gedit -y
 
