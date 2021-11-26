21e873b22707a24c2b650f18a84e4f4c570b5db3
Hieu Le <51386860+hieu2695@users.noreply.github.com>
2021-11-26 10:07:38 +0700

    Update package_installation_ubuntu.sh

--- package_installation_ubuntu.sh
+++ package_installation_ubuntu.sh
@@ -3,6 +3,7 @@ sudo apt-get install libjpeg-dev zlib1g-dev
 sudo apt-get install -y p7zip-full
 sudo apt install unzip
 sudo apt-get install gedit -y
+sudo apt-get install python3-gi-cairo
 
 # install CUDA 11.3 (Stable)
 pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
@@ -43,6 +44,7 @@ pip3 install joblib
 pip3 install dataclasses
 pip3 install typing
 python3 -m spacy download en
+pip3 install torchtext
 
 # install Plotly
 pip3 install plotly
