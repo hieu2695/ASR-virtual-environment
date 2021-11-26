908850c175f48e02bdb310468832cc55f7e32650
Hieu Le <51386860+hieu2695@users.noreply.github.com>
2021-11-26 08:35:52 +0700

    Update package_installation_ubuntu.sh

--- package_installation_ubuntu.sh
+++ package_installation_ubuntu.sh
@@ -1,5 +1,5 @@
 # install CUDA 11.3 (Stable)
-pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+sudo -H pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
 # install bacis python3 packages
 sudo apt install build-essential libssl-dev libffi-dev python3-dev -y
